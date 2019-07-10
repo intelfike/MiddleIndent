174ff7c7de527027c2bb8a9c5bb46f006f1ef424
intelfike <intelfike@gmail.com>
2019-07-10 15:36:34 +0900

    コメント挿入

--- MiddleIndent.py
+++ MiddleIndent.py
@@ -4,6 +4,7 @@ import sublime_plugin
 
 class MiddleIndentCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
+		# 一番右にあるカーソル位置を取得する
 		right_max = 0
 		for region in self.view.sel():
 			cursor_pos = region.b
@@ -11,6 +12,7 @@ class MiddleIndentCommand(sublime_plugin.TextCommand):
 			if left > right_max:
 				right_max = left
 
+		# すべてのカーソル位置に、一番右にあるカーソル位置に合わせて移動するようスペースを入れる
 		for region in self.view.sel():
 			cursor_pos = region.b
 			_, left = self.view.rowcol(cursor_pos)
